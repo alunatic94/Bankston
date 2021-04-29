e878611f01fa4aab277b8fd1e161661daf6652a2
Alberto <Alberto.luna.642@my.csun.edu>
2021-04-28 21:31:31 -0700

    added expand feature to welcome section

--- app.js
+++ app.js
@@ -28,11 +28,13 @@
 // }
 
 var expandDiv = document.getElementById("expand");
-var speed = 6;
+var expandDiv2 = document.getElementById("welcome-expand");
+var speed = 7;
 
 function expanding() {
   var scrolltop = window.pageYOffset; // get number of pixels document has scrolled vertically
   var scrollAndSpeed = (scrolltop / speed);
+  console.log("HEEEEY");
   //Expand using transform
   //expandDiv.style.transform = "scalex( " + Math.min(Math.max(scrollAndSpeed, 1), 10) + ")";
   
@@ -41,6 +43,23 @@ function expanding() {
 
 }
 
+function expanding2() {
+  var scrolltop = window.pageYOffset; // get number of pixels document has scrolled vertically
+  var scrollAndSpeed = (scrolltop / speed);
+  console.log("HEEEEY");
+  //Expand using transform
+  //expandDiv.style.transform = "scalex( " + Math.min(Math.max(scrollAndSpeed, 1), 10) + ")";
+  
+  //Or using width
+  expandDiv2.style.width = Math.min(Math.max(scrollAndSpeed, 20), 100) + "%";
+  expandDiv2.style.height = Math.min(Math.max(scrollAndSpeed, 20), 100) + "%";
+
+}
+
 window.addEventListener('scroll', function() { // on page scroll
   requestAnimationFrame(expanding); // call parallaxing()
+}, false);
+
+window.addEventListener('scroll', function() { // on page scroll
+  requestAnimationFrame(expanding2); // call parallaxing()
 }, false);
